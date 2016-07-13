79c72003d077819600ac9c5ebbb5ca311f0a95e6
biham <bidamon@gmail.com>
2016-07-13 16:55:57 +0000

    Finish user edit, update,index,and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
 gem 'bcrypt', '3.1.7'
+gem 'faker',  '1.4.2'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.1.0'
@@ -12,6 +13,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '3.2.0.0'
+gem 'will_paginate',  '3.0.7'
+gem 'bootstrap-will_paginate', '0.0.10'
 
 
 
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,6 +11,13 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
   
+  
+  # Returns true if teh given user is the current user
+  
+  def current_user?(user)
+    user == current_user
+  end
+  
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -41,5 +48,15 @@ module SessionsHelper
     forget(current_user)
     session.delete(:user_id)
     @current_user = nil
+  end
+  
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  # Strores the Url trying to be accessed
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
   end
 end
